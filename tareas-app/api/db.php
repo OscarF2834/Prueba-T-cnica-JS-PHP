4acf6f44bad98b7867d595183c115438eb0b0921
OscarF2834 <pipesegoviaalvarado@gmail.com>
2025-07-13 20:42:49 -0500

    ajuste tcp para arch

--- tareas-app/api/db.php
+++ tareas-app/api/db.php
@@ -1,4 +1,4 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=tareas', 'root', 'admin');
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=tareas', 'root', 'admin');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
